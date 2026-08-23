8e50837fce259465d4942e3fb4ee06fb844c79fd
Auguste <longwonderstaff@gmail.com>
2026-08-23 16:32:04 +0300

    lesson4 complete; add: models and migrations

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    text = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True)
