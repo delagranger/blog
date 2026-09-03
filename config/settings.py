85a0343c2e076fb0509cb89c2699c0b246c95449
Auguste <longwonderstaff@gmail.com>
2026-09-04 01:15:24 +0300

    feat: throttling

--- config/settings.py
+++ config/settings.py
@@ -65,6 +65,14 @@ REST_FRAMEWORK = {
         'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5,
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '1000/day',
+    },
 }
 
 SPECTACULAR_SETTINGS = {
